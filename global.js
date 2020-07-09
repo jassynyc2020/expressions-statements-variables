95c8631654d9bda1f86c629c130e6bac6197cbf9
Hubot <nishihaider@yahoo.com>
2020-07-09 01:29:23 -0400

    started
    not done

--- global.js
+++ global.js
@@ -6,15 +6,15 @@ console.log(4 + 5);
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here
+1 + 0;
 console.log("answer 1");
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here
+6 - 4;
 console.log("answer 2");
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here
+
 console.log("answer 3");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
